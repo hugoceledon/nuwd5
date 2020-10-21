82b40b3b5a053b6452ee09192492c97162d9bb50
Hugo Celedon <hugo.celedon@nextonia.com.co>
2020-10-21 16:59:55 -0500

    Añadidos comentarios y mejoras de forma.

--- js/board.js
+++ js/board.js
@@ -1,47 +1,84 @@
-var board = {
-    init: function(CMS, WMS) {
+/*!
+ * Board Game Object
+ * HUGO JOSÉ CELEDÓN FLÓREZ
+ * Next University
+ *
+ * Date: 2020-10-10T10:00
+ */
+
+// Board Object Declaration
+var boardGame = {
+    init: function(correctMoveScore, wrongMoveScore, timerMinutes) {
+        // Board initialization.
+
+        // Score configuration
+        this.correctMoveScore = correctMoveScore
+        this.wrongMoveScore = wrongMoveScore
+
+        // Timer configuration
+        this.Timer = timer // Timer object
+        this.Timer.init(timerMinutes * 60) // Begin timer object
+
+        // Initialization
         this.clear()
         this.movements = 0
         this.score = 0
         $(".elemento").draggable()
-        this.correctMoveScore = CMS
-        this.wrongMoveScore = WMS
         this.initResetBtn()
         this.blinkTitle(".main-titulo")
         this.blinkTitle(".game-over")
         this.bigScore = false; // Boolean to know if score is set to full screen
         this.endGame = false; // Boolean to know if game has ended or not
+
+    },
+    start: function() {
+        // Method to start the board.
+        self = this
+        setInterval(function() {
+            if (self.Timer.clockOver) {
+                self.stop();
+            }
+        }, 50)
+    },
+    stop: function() {
+        // Timer reached actions for the board.
+        console.log("Timer Reached!")
+        this.startAnimation()
+        this.endGame = true;
+        self.Timer.clockOver = false;
     },
     initResetBtn: function() {
+        // Set properties for reset button.
         var self = this
         $(".btn-reinicio").mouseup(function() {
             if (!self.endGame) {
-                if (!Timer.running) {
+                if (!self.Timer.running) {
                     $(this).html("Reiniciar")
-                    Board.fill()
-                    Timer.start()
+                    self.fill()
+                    self.Timer.start()
                     console.log("Started Game!")
                 } else {
-                    Timer.reset()
-                    Board.clear()
-                    Board.resetScore()
+                    self.Timer.reset()
+                    self.clear()
+                    self.resetScore()
                     $("#timer").css('color', '#E8CA06').css('font-size', '1.4em');
                     $(this).html("Iniciar")
-                    console.log("Reset the Timer!")
+                    console.log("Reseted Timer!")
                 }
             } else {
                 $("#timer").css('color', '#E8CA06').css('font-size', '1.4em');
-                Board.erase()
+                self.eraseColumns()
                 self.endGame = false;
-                Board.startAnimation();
+                self.startAnimation();
                 $(this).html("Iniciar")
-                Timer.init(Secs)
-                Board.resetScore()
+                self.Timer.restart()
+                self.resetScore()
                 console.log("Back to main event!")
             }
         })
     },
     fill: function() {
+        // Fills board with random elements.
         var self = this
         $(".panel-tablero").toggle("clip", 200, function() {
             for (var i = 1; i <= 7; i++) {
@@ -58,6 +95,7 @@ var board = {
         })
     },
     add: function(dict) {
+        // ? Add new random elements
         var self = this
         for (var i in dict) {
             if (dict.hasOwnProperty(i)) {
@@ -71,6 +109,7 @@ var board = {
         }
     },
     getColumnsDict: function(current) {
+        // Format current elements to a columns dictionary.
         var dict = new Object();
         for (i = 0; i < current.length; i++) {
             var res = current[i].split("-");
@@ -83,6 +122,7 @@ var board = {
         return dict
     },
     blink: function(current) {
+        // Blink animation when current elements are going to disapear.
         for (i = 0; i < current.length; i++) {
             $("#pos" + current[i]).animate({
                 opacity: 1.0,
@@ -102,21 +142,23 @@ var board = {
             })
         }
     },
-    removeElements: function(current) {
+    removeMatches: function(current) {
+        // Remove element matches.
         self = this
         self.score += self.correctMoveScore * current.length;
 
         self.blink(current)
 
-        var dict = self.getColumnsDict(current)
-            // REMOVE INVISIBLE ELEMENTS
+        var dict = self.getColumnsDict(current);
+        // REMOVE INVISIBLE ELEMENTS
         for (var key in dict) {
             if (dict.hasOwnProperty(key)) {
                 console.log(key, dict[key]);
                 var columnLength = dict[key].length
                 for (i = 0; i < columnLength; i++) {
                     var position = parseInt(dict[key][i].replace("-" + key, ""))
-                    console.log(position.toString() + " to " + (position - columnLength).toString())
+                    console.log(position)
+                        // console.log(position.toString() + " to " + (position - columnLength).toString())
                         // $("#pos" + current[i]).html("<img src=\"image/2.png\" class=\"elemento\"/>")
                 }
             }
@@ -125,6 +167,7 @@ var board = {
         self.add(dict)
     },
     setProps: function() {
+        // Set drag and drop properties to all element objects.
         self = this
         $(".elemento").draggable({ revert: true })
         $('.place').droppable({
@@ -152,7 +195,7 @@ var board = {
                         var current = self.analyzeCurrent()
                         if (current.length != 0) {
                             var current = self.analyzeCurrent()
-                            self.removeElements(current)
+                            self.removeMatches(current)
                         } else {
                             self.score -= self.wrongMoveScore;
                             Draggable.animate({
@@ -186,6 +229,7 @@ var board = {
         });
     },
     firstTimeClear: function() {
+        // Remove matches at first random fill of the board.
         self = this
         var current = self.analyzeCurrent()
         if (current.length != 0) {
@@ -199,6 +243,7 @@ var board = {
         }
     },
     analyzeCurrent: function() {
+        // Returns current matches to be changed.
         self = this
         var positions2Change = []
         for (var i = 1; i <= 7; i++) {
@@ -252,35 +297,40 @@ var board = {
         return positions2Change.flat()
     },
     clear: function() {
+        // Toggles the pannel and erases columns.
+        self = this
         $(".panel-tablero").toggle("clip", 200, function() {
-            for (var i = 1; i <= 7; i++) {
-                $(".col-" + i).html("")
-            }
+            self.eraseColumns()
             $(".panel-tablero").toggle("clip", 200)
         })
     },
-    erase: function() {
+    eraseColumns: function() {
+        // Erase all columns in the board.
         for (var i = 1; i <= 7; i++) {
             $(".col-" + i).html("")
         }
     },
     getXY: function(ID) {
+        // Get coordenates for given ID.
         var XY = ID.split("pos")[1].split("-")
         return [parseFloat(XY[0]), parseFloat(XY[1])]
     },
-    getImgKind: function(elemento) {
-        return parseFloat(elemento.split('\\').pop().split('/').pop().split('.')[0])
+    getImgKind: function(element) {
+        // Returns the image kind of an element.
+        return parseFloat(element.split('\\').pop().split('/').pop().split('.')[0])
     },
     resetScore: function() {
-        self = this
+        // Reset score and movements to zero.
         this.movements = 0
         this.score = 0
-        $("#movimientos-text").html(self.movements)
-        $("#score-text").html(self.score)
+        $("#movimientos-text").html(this.movements)
+        $("#score-text").html(this.score)
     },
-    startAnimation: function() { // Animation to set score full-screen or go back to default
+    startAnimation: function() {
+        // Animation to set score full-screen or go back to default.
         self = this
         if (self.bigScore) {
+            // If score is full screen.
             self.bigScore = false;
             $(".game-over").toggle("clip", 200)
             $(".panel-tablero").toggle("slide", 1, function() {
@@ -302,6 +352,7 @@ var board = {
                 })
             })
         } else {
+            // Change to fullscreen score.
             self.bigScore = true;
             $(".panel-tablero").animate({
                 width: "0%"
@@ -318,6 +369,7 @@ var board = {
         }
     },
     blinkTitle: function(title) {
+        // Change color to white and 1000 milliseconds to yellow.
         self = this
         $(title).animate({
             color: "#FFF"
@@ -326,6 +378,7 @@ var board = {
         });
     },
     yellowTitle: function(title) {
+        // Change color to yellow and 600 milliseconds back to white.
         self = this
         $(title).animate({
             color: "#DCFF0E"
